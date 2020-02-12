5cd073b4ff9b074d135d220d6fbf052b4a3697d8
Taiyi Pan <taiyipan@gmail.com>
2020-02-11 17:50:42 -0800

    Added shell scripts for quick serving and inspecting

---
+++ inspect_saved_model.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+saved_model_cli show \
+--dir cats_dogs_classifier/02 --all
---
+++ run_model__server.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+docker run -it --rm \
+-p 8500:8500 \
+-p 8501:8501 \
+-v "$(pwd)/cats_dogs_classifier:/models/cats_dogs" \
+-e MODEL_NAME=cats_dogs \
+tensorflow/serving
