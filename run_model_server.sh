1c4c6ddd94b1d254e811d120d06f85a9a508a7df
Taiyi Pan <taiyipan@gmail.com>
2020-02-11 18:13:41 -0800

    more

--- run_model_server.sh
+++ run_model_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --rm -p 8500:8500 -p 8501:8501 -v "$(pwd)/cats_dogs_classifier:/models/cats_dogs" -e MODEL_NAME=cats_dogs tensorflow/serving
+docker run -it --rm -p 8500:8500 -p 8501:8501 -v "cats_dogs_classifier:/models/cats_dogs" -e MODEL_NAME=cats_dogs tensorflow/serving
